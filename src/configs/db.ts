d701968632e012c5800ca4e08558a728e92c22dd
Abdur Rahman <19cse040@gmail.com>
2025-10-04 13:20:03 +0600

    add new url for mongodb

--- src/configs/db.ts
+++ src/configs/db.ts
@@ -4,7 +4,7 @@ import { getErrorMessage } from "../helpers/config.helper";
 const connectDB = async (): Promise<void> => {
   try {
     mongoose.connection.on("connected", () => console.info("☁️  Database connected"));
-    await mongoose.connect(`${process.env.MONGODB_URI}/Startech-core-backend`);
+    await mongoose.connect(`${process.env.MONGODB_URI}/Startech-backend`);
   } catch (error: unknown) {
     console.error(getErrorMessage(error));
     process.exit(1); // stop server if DB fails
